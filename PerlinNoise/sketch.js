e2f73680697b5718b1aa54f2d6eb335d0c35369f
CNova <caiocnova1@gmail.com>
2020-07-30 10:21:09 -0300

    Add Comparation between random and noise functions

---
+++ PerlinNoise/sketch.js
@@ -0,0 +1,17 @@
+let xoff = 0;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(51);
+  // let x = random(width);
+  let y = random(width);
+  let x = map(noise(xoff), 0, 1, 0, width);
+  xoff += 0.01;
+
+  
+  ellipse(x, 200, 24, 24);
+  ellipse(y, 300, 24, 24);
+}
