632ff18208e66f8fad19b2a2e6d00418239a118e
CNova <caiocnova1@gmail.com>
2020-07-30 10:29:52 -0300

    Separate forlders

--- PerlinNoise/randomNoiseComp/sketch.js
+++ PerlinNoise/randomNoiseComp/sketch.js
@@ -6,11 +6,13 @@ function setup() {
 
 function draw() {
   background(51);
-  // let x = random(width);
-  let y = random(width);
-  let x = map(noise(xoff), 0, 1, 0, width);
-  xoff += 0.01;
 
+  // One Dimension Perlin Noise Position Ball
+  let x = map(noise(xoff), 0, 1, 0, width);
+  xoff += 0.02;
+
+  // One Dimension Random Position Ball
+  let y = random(400);
   
   ellipse(x, 200, 24, 24);
   ellipse(y, 300, 24, 24);
