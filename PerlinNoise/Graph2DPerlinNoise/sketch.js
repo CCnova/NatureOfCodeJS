3fd30adf79c4d0efca31994f237108ec86deec9d
CNova <caiocnova1@gmail.com>
2020-07-30 11:23:21 -0300

    add television noise

---
+++ PerlinNoise/Graph2DPerlinNoise/sketch.js
@@ -0,0 +1,24 @@
+let inc = 0.01;
+
+function setup() {
+  createCanvas(200, 200);
+  pixelDensity(1);
+}
+
+function draw() {
+  loadPixels();
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      let index = (x + y * width) * 4;
+      let r = random(255);
+      pixels[index] = r;
+      pixels[index + 1] = r;
+      pixels[index + 2] = r;
+      pixels[index + 3] = 255;
+    }
+  }
+
+  updatePixels();
+
+  // noLoop();
+}
